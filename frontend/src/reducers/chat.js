85ae58c53750bbd605bc41e863b70a924ea52796
Henrik Qvigstad <henrik.qvigstad@gmail.com>
2020-05-17 14:55:19 +0200

    techincal test changes

--- frontend/src/reducers/chat.js
+++ frontend/src/reducers/chat.js
@@ -5,7 +5,7 @@ import {
 import * as api from "../utils/api";
 
 
-const ACTIONS = defineActions('chat', ['LOAD_REQUEST', 'LOAD_SUCCESS', 'LOAD_FAILURE']);
+const ACTIONS = defineActions('chat', ['LOAD_REQUEST', 'LOAD_SUCCESS', 'LOAD_FAILURE', 'POST_MESSAGE']);
 
 const initialState = {
   ...HTTP.INIT,
@@ -20,6 +20,13 @@ export default function (state = initialState, action) {
       return { ...state, ...HTTP.SUCCESS, chat: action.chat };
     case ACTIONS.LOAD_FAILURE:
       return { ...state, ...HTTP.FAILURE, error: action.error };
+    case ACTIONS.POST_MESSAGE:
+      const chat = {
+        ...state.chat, messages: [
+          ...state.chat.messages, { user: 'placeholder', message: action.message }
+        ]
+      }
+      return { ...state, chat }
     default:
       return state;
   }
@@ -31,6 +38,15 @@ export const loadChat = (chatId) => async dispatch => {
   try {
     const chat = await api.getJSON(`chat/${chatId}`);
     dispatch({ type: ACTIONS.LOAD_SUCCESS, chat });
+  } catch (err) {
+    dispatch({ type: ACTIONS.LOAD_FAILURE, error: "Something went wrong." });
+  }
+}
+
+export const postMessage = (message, chatId) => async dispatch => {
+  try {
+    const response = await api.post(`chat/${chatId}/message`, { message });
+    dispatch({ type: ACTIONS.POST_MESSAGE, message });
   } catch (err) {
     dispatch({ type: ACTIONS.LOAD_FAILURE, error: "Something went wrong." });
   }
--- frontend/src/views/Chat.js
+++ frontend/src/views/Chat.js
@@ -1,15 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { loadChat, postMessage } from '../reducers/chat';
 // import { useParams } from 'react-router-dom';
 
 
+const useChat = (chatId) => {
+    const dispatch = useDispatch();
+    const state = useSelector(state => state.chat)
+    const { pending, success, failure } = state;
+
+    useEffect(() => {
+        if (!pending && !success && !failure) dispatch(loadChat(chatId));
+    }, [pending, success, failure, dispatch]);
+
+    return state;
+}
+
 const Chat = () => {
     // need to upgrade react-router-dom to use userParams
-    // const { id: chatId } = useParams();
+    // const { id: chatId } = useParams();    
+    const urlParts = document.location.href.split('/');
+    const chatId = urlParts[urlParts.length - 1];
+    const { chat, loading } = useChat(chatId);
+
+    const dispatch = useDispatch();
+    const [messageInput, setMessageInput] = useState('');
+    const handleSubmit = evt => {
+        evt.preventDefault()
+        dispatch(postMessage(messageInput, chatId))
+    }
+
+    if (loading || !chat) return 'Loading...';
+    console.log('debug Chat', { chat })
     return (
         <div className="container">
             <div className="row">
                 <div className="col-sm-12 col-md-10 question-list">
-                    TODO Chat
+                    <ol>
+                        {chat.messages.map((message) => {
+                            return (
+                                <li key={message['_id']}>
+                                    {`${message.user}: ${message.message}`}
+                                </li>
+                            );
+                        })}
+                    </ol>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-12 col-md-10 question-list">
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-group">
+                            <label htmlFor="post-input">post</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="post-input"
+                                value={messageInput}
+                                onChange={(e) => setMessageInput(e.target.value)}
+                            />
+                        </div>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+
                 </div>
             </div>
         </div>
